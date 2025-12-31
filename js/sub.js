0bb5f91843f288ac409ed52a0d70d255b7c9c9f3
hgonnnnn <hgonnnnn5@gmail.com>
2025-12-31 15:35:27 +0900

    20251231_15:35

---
+++ js/sub.js
@@ -0,0 +1,16 @@
+$(function(){
+    /* 스크롤 시 부드럽게 */
+    const lenis = new Lenis({
+        duration: 1.2,   // 스크롤 속도
+        smooth: true,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    })
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+    /* aos */
+    AOS.init();
+});
