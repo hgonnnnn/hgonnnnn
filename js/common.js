fcfe6a5584840b2a260cd8226af38aa28e0d56c6
hgonnnnn <hgonnnnn5@gmail.com>
2025-12-31 16:03:03 +0900

    20251231_16:03

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#header').load('common/header.html');
-    $('#footer').load('common/footer.html');
+    $('#header').load('/hgonnnnn/common/header.html');
+    $('#footer').load('/hgonnnnn/common/footer.html');
 });
 /* 깃허브 업로드 시 / 삭제 */
--- sub/js/common.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $('#header').load('../common/header.html');
-    $('#footer').load('../common/footer.html');
-});
-/* 깃허브 업로드 시 / 삭제 */
